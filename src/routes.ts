96b682916097d645fbfa82858bbce123fc1ceb5b
Garrett Downs <garredow@outlook.com>
2022-07-26 01:18:07 -0400

    handle errors

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,28 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { getHealth } from './controllers/health';
 import { getCurrentUser } from './controllers/user';
+
+const handleErrors =
+  (controllerFn: (request: FastifyRequest, reply: FastifyReply) => any) =>
+  (request: FastifyRequest, reply: FastifyReply) =>
+    controllerFn(request, reply).catch((err: any) => {
+      request.log.error(err);
+      reply.code(500).send({
+        statusCode: 500,
+        error: 'API Error',
+        message: '',
+      });
+    });
 
 export async function routes(fastify: FastifyInstance) {
   // User
   fastify.get('/me', {
     preValidation: fastify.authenticate,
-    handler: getCurrentUser,
+    handler: handleErrors(getCurrentUser),
   });
 
   // Health
   fastify.get('/health', {
-    handler: getHealth,
+    handler: handleErrors(getHealth),
   });
 }
